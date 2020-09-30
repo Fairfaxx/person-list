57653dae6af1381e8189a28d90b483f369da8590
Fede Lemaire <fede.lemaire@gmail.com>
2020-09-30 08:47:22 -0300

    need to be responsive yet

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import axios from 'axios';
+
+const Person = ({ img, name, job }) => {
+
+  const [people, setPeople] = useState([])
+
+
+  useEffect(() => {
+    const getPeople = async () => {
+      const data = await axios.get('https://randomuser.me/api/');
+      console.log(data.data.results)
+      setPeople(data.data.results)
+    }
+
+    getPeople()
+  }, [])
+
+  return (
+
+
+    <div>
+      <ul className='person-list'>
+        {
+          people.map((person) => {
+
+            return (
+              <li className='person' key={person.id}>
+                <img src={person.picture.large} alt={person.name} />
+                <div>
+                  <h2>{person.name.first}</h2>
+                  <h2>{person.name.last}</h2>
+                  <h4>{`Age:${person.dob.age}`}</h4>
+                  <h4>{person.email}</h4>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+
+
+  )
+}
+
+
+const PersonList = () => {
+  return (
+    <section className="person-list">
+      <Person img='37' name='Fefe' job='Developer' />
+      <Person img='15' name='Jeffrey' job='Developer' />
+      <Person img='19' name='Richard' job='Developer' />
+      <Person img='19' name='pedro' job='Developer' />
+      <Person img='19' name='Pablo' job='Developer' />
+    </section>
+  )
+}
+
+ReactDOM.render(<PersonList />, document.getElementById('root'));
